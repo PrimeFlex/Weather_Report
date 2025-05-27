fee18d1fbd588cb81d566975e7a90ffe61375539
PrimeFlex <jeremy@primeflex-adjusters.com>
2025-05-27 14:17:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 from openweather import get_weather_summary
 from noaa_parser import search_noaa_storms
 from generate_report import create_weather_report
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENWEATHER_API_KEY")
 
 # --- Sample Inputs (will be dynamic later) ---
 zip_code = "45231"
 date_of_loss = "2024-04-20"
 lat = 39.2334  # Example: Cincinnati, OH
 lon = -84.5545
-api_key = "REMOVED_API_KEY"
 
 # --- Weather and Storm Lookups ---
 owm_data = get_weather_summary(lat, lon, date_of_loss, api_key)
