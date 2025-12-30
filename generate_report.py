e738ed238bb90fd775f082ec9986fc2b3d9089cf
Jeremy Perry <jeremy@primeflex-adjusters.com>
2025-12-30 13:33:01 -0500

    Rename parameter 'date_of_loss' to 'date_of_storm'

--- generate_report.py
+++ generate_report.py
@@ -1,3 +1,3 @@
 
-def create_weather_report(zip_code, date_of_loss, owm_data, noaa_data):
-    return f"Zip: {zip_code}, Date: {date_of_loss}, Weather: {owm_data}, Storms: {noaa_data}"
+def create_weather_report(zip_code, date_of_storm, owm_data, noaa_data):
+    return f"Zip: {zip_code}, Date: {date_of_storm}, Weather: {owm_data}, Storms: {noaa_data}"
